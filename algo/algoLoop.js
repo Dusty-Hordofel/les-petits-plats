e3dafd50db001a2781743fe0d9d1da9f27c5f422
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2023-05-03 05:46:49 +0200

    Fiche d'investigation et performances

--- algo/algoLoop.js
+++ algo/algoLoop.js
@@ -1,77 +1,76 @@
 let ingredientsArray = (recipes, inputValue) => {
-  // console.log(recipes);
   let allIngredients = [];
-  //   let ingredientInput = inputValue.toLowerCase().trim();
+  let ingredientInput = inputValue.toLowerCase().trim();
 
   for (const recipe of recipes) {
-    // console.log(recipe);
     for (const ingredient of recipe.ingredients) {
       let currentIngredient = ingredient.ingredient.toLowerCase().trim();
-      // console.log(currentIngredient);
       if (allIngredients.length === 0) {
         allIngredients.push(currentIngredient);
       } else {
-        let isIn = false;
+        let isKeywordIn = false;
         for (const itemInIngredients of allIngredients) {
           if (itemInIngredients === currentIngredient) {
-            isIn = true;
+            isKeywordIn = true;
           }
         }
-        if (!isIn) {
+        if (!isKeywordIn) {
           allIngredients.push(currentIngredient);
         }
       }
     }
   }
 
-  // SI RECHERCHE DANS INPUT....
-  //   if (inputValue) {
-  //     return allIngredients.filter((ingredient) =>
-  //       ingredient.includes(ingredientInput.toLowerCase().trim())
-  //     );
-  //   }
-  // SANS RECHERCHE
+  // Si on a un element dans l'input, on filtre les ingrédients
+  if (inputValue) {
+    return allIngredients.filter((ingredient) =>
+      ingredient.includes(ingredientInput.toLowerCase().trim())
+    );
+  }
+  // Sinon on retourne tous les ingrédients
   return allIngredients;
 };
 
+console.log(
+  "🚀 ~ file: algoLoop.js:1780 ~ ingredientsArray ~ ingredientsArray:",
+  ingredientsArray(allRecipesData, "Lait de coco")
+);
+
 let ustensilsArray = (recipes, inputValue) => {
-  // console.log(recipes);
   let allUstensils = [];
-  //   let ustensilInput = inputValue.toLowerCase().trim();
+  let ustensilInput = inputValue.toLowerCase().trim();
 
   for (const recipe of recipes) {
-    // console.log(recipe);
     for (const ustensil of recipe.ustensils) {
       let currentUstensil = ustensil.toLowerCase().trim();
       if (allUstensils.length === 0) {
         allUstensils.push(currentUstensil);
       } else {
-        let isIn = false;
+        let isKeywordIn = false;
         for (const itemInUstensils of allUstensils) {
           if (itemInUstensils === currentUstensil) {
-            isIn = true;
+            isKeywordIn = true;
           }
         }
-        if (!isIn) {
+        if (!isKeywordIn) {
           allUstensils.push(currentUstensil);
         }
       }
     }
   }
 
-  // SI RECHERCHE DANS INPUT....
-  //   if (inputValue) {
-  //     return allUstensils.filter((ustensil) =>
-  //       ustensil.includes(ustensilInput.toLowerCase().trim())
-  //     );
-  //   }
-  // SANS RECHERCHE
+  // Si on a un element dans l'input, on filtre les ustensils
+  if (inputValue) {
+    return allUstensils.filter((ustensil) =>
+      ustensil.includes(ustensilInput.toLowerCase().trim())
+    );
+  }
+  // Sinon on retourne tous les ustensils
   return allUstensils;
 };
 let appliancesArray = (recipes, inputValue) => {
-  // console.log(recipes);
   let allAppliances = [];
-  //   let applianceInput = inputValue.toLowerCase().trim();
+  let applianceInput = inputValue.toLowerCase().trim();
 
   for (const recipe of recipes) {
     const currentAppliance = recipe.appliance.toLowerCase().trim();
@@ -79,18 +78,14 @@ let appliancesArray = (recipes, inputValue) => {
       allAppliances.push(currentAppliance);
     }
   }
-  // SI RECHERCHE DANS INPUT....
-  //   if (inputValue) {
-  //     return allAppliances.filter((ustensil) =>
-  //       ustensil.includes(applianceInput.toLowerCase().trim())
-  //     );
-  //   }
-  // SANS RECHERCHE
+  //Si on a un element dans l'input, on filtre les appareils
+  if (inputValue) {
+    return allAppliances.filter((ustensil) =>
+      ustensil.includes(applianceInput.toLowerCase().trim())
+    );
+  }
+  // Sinon on retourne tous les appareils
   return allAppliances;
 };
 
 getStorageItem("recipes");
-
-// console.log(ingredientsArray(getStorageItem("recipes")));
-// console.log(ustensilsArray(getStorageItem("recipes")));
-// console.log(appliancesArray(getStorageItem("recipes")));
--- components/recipeDescription.js
+++ components/recipeDescription.js
@@ -1,39 +1,19 @@
-// recipeDescription(ingredients)
-//setRecipesCards
-const setRecipesCards = (element, recipes) => {
-  element.innerHTML = `<ul class="recipes__container">
-    ${recipes
-      .map((recipe) => {
-        const { id, name, ingredients, time, description } = recipe;
-        return `<li class="recipe__card" id="${id}">
-        <article className="recipe__card" id={id}>
-        <div class="recipe__card__header">
-          <img
-            src="assets/images/img.png" alt="blank image"
-            alt={recipe.title}
-            class="recipe__card__header--image"
-          />
-          </div>
-          <div class="recipe__card__description">
-          <div class="recipe__card__description--title flex-row">
-          <p id="recipe__title">
-          ${name}
-          </p>
-          <p id="recipe__time">
-          <span class="fa-regular fa-clock">  ${time}</span>  min
-          </p>
-          </div>
-          <div class="recipe__card__description__ingredients--container">
-        <ul class="recipe__card__description__ingredients">
-        ${recipeDescription(ingredients)}
-        </ul>
-        <div class="recipe__card__description__ingredients--details">${description}</div>
-        </div>
-
-      </article>
-        </li>`;
-      })
-      .join("")}
-    </ul>
-    </div>`;
+const recipeDescription = (ingredients) => {
+  return ingredients
+    .map((ingrediento) => {
+      const { ingredient, quantity, unit } = ingrediento;
+      return `
+    <li class="recipe__card__description__ingredients--item">
+    <p class="recipe__card__description--ingredients__item--name"><b>${
+      ingredient ? ingredient : ""
+    }</b>${quantity ? " : " : " "}</p>
+    <p class="recipe__card__description--ingredients__item--quantity">${
+      quantity ? quantity : ""
+    }</p>
+    <p class="recipe__card__description--ingredients__item--unit">${
+      unit ? unit : ""
+    }</p>
+    </li>`;
+    })
+    .join("");
 };
